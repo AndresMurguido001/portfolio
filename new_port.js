3b92793ed4c89d87310303055b63e79efa20cac2
Your Name <dre.murguido@gmail.com>
2018-02-26 15:26:36 -0500

    Added TypeIt

--- new_port.js
+++ new_port.js
@@ -1,5 +1,10 @@
+// import TypeIt from 'typeit';
 
-var typed = new Typed('.element', {
-  strings: ["First sentence.", "Second sentence."],
-  typeSpeed: 30
+$(document).ready(function(){
+  $('#welcome').typeIt({
+    speed: 100,
+  }).tiType("Developer Profile").tiPause(750).tiEmpty().tiType("Designer")
+  .tiPause(750).tiEmpty().tiType("Artist")
+
+
 });
