f90fb72909b12368eaf32352e578de51b7e7cde7
Your Name <dre.murguido@gmail.com>
2018-03-18 13:13:31 -0400

    initial commit for new portfolio

---
+++ public/new_port.js
@@ -0,0 +1,17 @@
+// import TypeIt from 'typeit';
+
+$(document).ready(function(){
+  $('#welcome').typeIt({
+    speed: 100,
+  }).tiType("Developer Profile").tiPause(750).tiEmpty().tiType("Designer")
+  .tiPause(750).tiEmpty().tiType("Artist")
+
+});
+
+// document.addEventListener('DOMContentLoaded', function(){
+//   var button = document.getElementById('button');
+//   button.addEventListener('click', function(){
+//     document.forms[0].submit();
+//   })
+
+})
