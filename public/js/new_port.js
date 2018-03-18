41610beec15304b5513bf2f0722d90d15f83ba67
Your Name <dre.murguido@gmail.com>
2018-03-18 15:43:11 -0400

    Added drivewaze to projects dropdown

--- public/js/new_port.js
+++ public/js/new_port.js
@@ -6,12 +6,5 @@ $(document).ready(function(){
   }).tiType("Developer Profile").tiPause(750).tiEmpty().tiType("Designer")
   .tiPause(750).tiEmpty().tiType("Artist")
 
+
 });
-
-// document.addEventListener('DOMContentLoaded', function(){
-//   var button = document.getElementById('button');
-//   button.addEventListener('click', function(){
-//     document.forms[0].submit();
-//   })
-
-})
